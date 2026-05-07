20282739ef246ae44fef7c64300085f2e5269788
samia-majdi <samiamajdi.m@gmail.com>
2026-05-07 10:41:34 +0100

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,6 @@ dependencies {
     implementation(libs.micronaut.http.server.netty)
     implementation(libs.micronaut.serde.jackson)
     implementation(libs.micronaut.views.thymeleaf)
-    // The app uses Micronaut Data JDBC (not Hibernate/JPA).
     implementation(libs.micronaut.data.jdbc)
     implementation(libs.micronaut.jdbc.hikari)
     implementation(libs.micronaut.validation)
